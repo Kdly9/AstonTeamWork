33d88832905de1ff64b30de60079b62ebd9e5421
a.gaifullina <nastya-25-97@mail.ru>
2026-09-09 22:31:57 +0700

    Создала класс ConventionVisitor для работы

---
+++ src/main/java/TicketType.java
@@ -0,0 +1,23 @@
+public enum TicketType {
+    VIP(1, "VIP"),
+    OPEN_DATE(2, "Билет с открытой датой"),
+    THREE_DAYS(3, "Билет на три дня"),
+    ONE_DATE(4, "Однодневный билет"),
+    SUPER_VIP(0, "Super VIP");
+
+    private final int priority;
+    private final String displayName;
+
+    TicketType(int priority, String displayName) {
+        this.priority = priority;
+        this.displayName = displayName;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
